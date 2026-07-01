75bb748882f06895f66f4444d1368c52d13dcc1d
anildt123 <tembhare.anil@gmail.com>
2026-07-02 01:18:57 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,20 @@
+# no=int(input("Please enter a no"))
+# factorial=1
+
+# if no < 0:
+#     print(f" Factorial does not less than zero no")
+# elif  no==0:
+#     print(f" Factorial does not equal to zero no")
+# else:
+#     for i in range(1,no+1):
+#         factorial=factorial*i
+#         print (f"The factorail of {no} is {factorial}") 
+#         break 
+# Write a Python Program to Display the multiplication Table.
+num=int(input("Please Enter no  "))
+for i in range(1,11):
+   print(f" Tha table of no {num} is {num} X {i}={num*i}")
+#for i in range(1,11):
+
+        #print(f"{num}X{i}={num*i}")
+
