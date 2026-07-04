b1c47165edcca1d3a8bbd42736bc45c87326a5df
anildt123 <tembhare.anil@gmail.com>
2026-07-05 01:15:53 +0530

    This is my first commit

---
+++ 1_Add_div_nos.py
@@ -0,0 +1,15 @@
+#Write a program to do arithmatical operation Add and div
+num1= int(input("Enter the value of num1"))
+num2= int(input("Enter the value of num2"))
+sum=num1+num2
+print (f" num1+num12,{num1}+{num2}={sum}")
+num3= float(input("Enter the Divident for division"))
+num4= float(input("Enter the Divisor for division"))
+if num4==0:
+    print("Error : Division by zero is not allowed")
+else :
+    div_result=num3/num4
+    print(f"Division of num3,num4 is {num3} / {num4} Ans is {div_result} " )
+
+
+
